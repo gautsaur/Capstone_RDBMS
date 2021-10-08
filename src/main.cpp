44f4827e587b8ffae7fe69847bd83c50680be887
aamijani <aamijani@isu.edu>
2021-10-07 21:08:24 -0600

    Completed Create Table Function

--- src/main.cpp
+++ src/main.cpp
@@ -35,22 +35,48 @@ int main(int argc, char** argv) {
 	while(to_lower(cmd) != "exit") {
 		cmd = "";
 
+<<<<<<< Updated upstream
+=======
+		// Setup the command to wait for input
+>>>>>>> Stashed changes
 		color(10);
 
 		if(current_db_name.length() > 0) {
 			std::cout << current_db_name << "@";
 		}
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 		std::cout << "SQL>";
 		color(7);
 		std::getline(std::cin, cmd);
 
+<<<<<<< Updated upstream
 		// Do something with cmd
 		if(tolower(cmd.find("open database ") == 0)){
 			current_db_name = cmd.substr(cmd.find_last_of(' ') + 1, cmd.find_last_of(';') - cmd.find_last_of(' ') - 1);
 
+=======
+		// SELECT [ID, TEST, ] FROM TABLE;
+
+		// Do something with cmd
+		if(tolower(cmd.find("open database ") == 0)){
+			current_db_name = cmd.substr(cmd.find_last_of(' ') + 1, cmd.find_last_of(';') - cmd.find_last_of(' ') - 1);
+			Database *db = new Database(current_db_name);
+
+			if(db->database_name != current_db_name){
+				current_db_name = "";
+			}
+
+		} else if (to_lower(cmd) == "help") {
+			show_help();
+		} else if (to_lower(cmd) == "list database") {
+			Database::List();
+>>>>>>> Stashed changes
 		} else if(tolower(cmd.find("create table ") == 0)){
 		    table_name = cmd.substr(cmd.find_last_of(' ' ) + 1, cmd.find_last_of(';') - cmd.find_last_of(' ') - 1);
+		    //Table *tbl = new create_table(current_db_name, table_name, );
 
 		} else if (to_lower(cmd) == "exit"){
 			std::cout << "Good Bye" << std::endl;
@@ -58,8 +84,11 @@ int main(int argc, char** argv) {
 			std::cout << "Invalid Command." << std::endl;
 		}
 
+<<<<<<< Updated upstream
 		example_write();
 
+=======
+>>>>>>> Stashed changes
 	}
 
 	//Database *db = new Database("test");
@@ -69,12 +98,37 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
+<<<<<<< Updated upstream
+=======
+/// Converts a string to lower
+>>>>>>> Stashed changes
 std::string to_lower(std::string s){
 	std::for_each(s.begin(), s.end(), [](char & c){
     	c = ::tolower(c);
 	});
+<<<<<<< Updated upstream
 
 	return s;
+=======
+
+	return s;
+}
+
+/// Shows the help menu
+void show_help() {
+	std::cout << "Available Commands:" << std::endl;
+	std::cout << "OPEN DATABASE 		- Check if the database exists and open it." << std::endl;
+	std::cout << "CREATE DATABASE 	- Creates and new database and opens a connection to it." << std::endl;
+	std::cout << "DROP DATABASE 		- Deletes the given database." << std::endl;
+	std::cout << "CREATE TABLE 		- Creates a table in the current database." << std::endl;
+	std::cout << "DROP DATABASE 		- Check if the database exists and open it." << std::endl;
+	std::cout << "SELECT [] FROM [] 	- Selects the specified columns from the table." << std::endl;
+	std::cout << "UPDATE TABLE 		- Updates the columns and meta for the given table." << std::endl;
+	std::cout << "DELETE FROM 		- Deletes the sepcified data from the table." << std::endl;
+	std::cout << "INSERT INTO 		- Inserts the data into the table." << std::endl;
+	std::cout << "LIST DATABASE 		- Lists the current database names." << std::endl;
+
+>>>>>>> Stashed changes
 }
 
 void setup_intro() {
@@ -91,13 +145,18 @@ void setup_intro() {
 	Sleep(400);
 	std::cout << ".";
 
+<<<<<<< Updated upstream
 	std::cout << std::endl << std::endl << "Success! Here is your shell. Type exit to quit." << std::endl << std::endl;
+=======
+	std::cout << std::endl << std::endl << "Success! Here is your shell." << std::endl << "Type [help] for a list of commands. Type [exit]to quit." << std::endl << std::endl;
+>>>>>>> Stashed changes
 }
 
 void color( int s){
 	SetConsoleTextAttribute( h, s );
 }
 
+<<<<<<< Updated upstream
 void example_write()
 {
 	// (create database) (test);
@@ -147,12 +206,21 @@ void example_read(std::string db_name) {
 }
 
 Table* create_table(Database *db, std::string table_name){
+=======
+Table* create_table(Database *db, std::string table_name, std::map<std::string, std::string> columns){
+>>>>>>> Stashed changes
     // (create table) (test_table) (id int, name string)
     Table *tbl = new Table(table_name);
 
     // Set the columns
-    tbl->columns.insert({"int", "id"});
-	tbl->columns.insert({"string", "name"});
+    for(std::map<std::string, std::string>::iterator it = columns.begin(); it != columns.end(); ++it) {
+        std::string key = it->first;
+        std::string value = it->second;
+        tbl->columns.insert({key, value});
+    }
+
+    //tbl->columns.insert({"int", "id"});
+	//tbl->columns.insert({"string", "name"});
 
 	// insert into test_table (id, name) values((1, "test 1"), (2, "test 2"))
 	// Insert the rows
